e2fb3f180aaa7eecfe55f98ce58e33ac73a041b7
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2021-09-26 15:14:02 -0400

    Ajout classe algo NI

---
+++ AlgoNonInforme.py
@@ -0,0 +1,10 @@
+#Obtenir la position de l'aspirateur
+#Créer un arbre de recherches et un stockage
+#Analyser l'état de la pièce actuelle
+    #S'il y a de la poussière > aspirer
+    #S'il y a un bijou > le ramasser
+    #Sinon récursivité :
+    #Chercher les pièces juxtaposées jamais explorées et en créer des noeuds à ajouter à l'arbre et au stockage
+    #Répéter jusqu'à trouver une pièce sale puis retourner l'arbre créé
+    #En utilisant l'arbre déplacer le robot puis aspirer ou ramasser
+    #Répéter
