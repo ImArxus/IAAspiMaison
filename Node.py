8dc4de3cbf982ca74528cafcff90b04ce8ad1055
ImArxus <alex.lebalanger@icloud.com>
2021-09-27 16:20:53 -0400

    A* & expand function drafts

--- Node.py
+++ Node.py
@@ -6,6 +6,8 @@ class Node:
         self.down = None
         self.left = None
         self.right = None
+        self.depth = 0
+        self.parent = None
 
     def __str__(self):
         return '( {self.data} , up : {self.up}, down : {self.down}, left : {self.left}, right : {self.right})'.format(self=self)
@@ -22,4 +24,14 @@ class Node:
             self.left.parent = self
         else:
             self.right = Node(data)
-            self.right.parent = self  
+            self.right.parent = self
+
+    def expand(self, grid, robot):
+        successors = []
+        actions = []
+        for action in actions:
+            s = Node(self, self.make_action(action, grid), action,
+                     self.depth+1, self.cost_action(action)+self.parent.get_cost())
+            self.affect_heuristique(robot)
+            successors.append(s)
+        return successors
--- Robot.py
+++ Robot.py
@@ -1,23 +1,53 @@
+from Node import Node
+
+
 class Robot:
     def __init__(self, posX, posY):
         self.posX = posX
         self.posY = posY
 
+    def get_positionX(self):
+        return self.posX
+
+    def get_positionY(self):
+        return self.posY
+
+    def set_positionX(self, X):
+        self.posX = X
+
+    def set_positionY(self, Y):
+        self.posY = Y
+
     def __str__(self):
         return 'Le robot est situé à l emplacement :  {self.posX} , {self.posY}'.format(self=self)
 
     def moveLeft(self):
-        if self.posX >0:
-            self.posX=self.posX-1
-    
+        if self.posX > 0:
+            self.posX = self.posX-1
+
     def moveRight(self):
-        if self.posX <4:
-            self.posX=self.posX+1
-    
+        if self.posX < 4:
+            self.posX = self.posX+1
+
     def moveUp(self):
-        if self.posY >0:
-            self.posY=self.posY-1
+        if self.posY > 0:
+            self.posY = self.posY-1
 
     def moveDown(self):
-        if self.posY <4:
-            self.posY=self.posY+1
+        if self.posY < 4:
+            self.posY = self.posY+1
+
+    def aStar(self, grid) -> Node:
+        node_start = Node(grid.getCell(
+            self.get_positionX(), self.get_positionY()))
+        node_start.affect_heuristique(self)
+        node_list = []
+        node_list.append(node_start)
+        while self.goal_reached(node_list[0]) == False:
+            node_tmp = node_list[0]
+            node_list.remove(node_list[0])
+            new_nodes = node_tmp.expand(self.get_belief(), self)
+            for node in new_nodes:
+                node_list.append(node)
+            node_list.sort()
+        return node_list[0]
