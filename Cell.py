0de6d2dc4075545fe5025ca8b2d1a4d801f1d8c2
Marmorand31 <47538734+Marmorand31@users.noreply.github.com>
2021-09-28 19:32:53 -0400

    fonction distance et heuristique

--- Cell.py
+++ Cell.py
@@ -11,7 +11,7 @@ class Cell:
 
     def get_dust(self):
         return self.dust
-    
+
     def get_jewel(self):
         return self.jewel
 
@@ -32,3 +32,21 @@ class Cell:
 
     def set_jewel(self, jewel):
         self.jewel = jewel
+
+    def distance(self, cellPosX,
+                 cellPosY):  ##----- Fonction qui indique la distance d'une cellule dont on lui fournit les coordonnées jusqu'a elle même -----#
+        distX = self.posX - cellPosX
+        distY = self.posY - cellPosY
+        distTot = abs(distX) + abs(distY)
+        return distTot  ##--- distance en nombre de déplacement total---##
+
+    def heuristique(self, listCells): #--- On obtient l'element de la liste le plus proche de la case où on est ---#
+        bestCaseToGo = 25
+        lowestNbMovement = 11
+        i = 0
+        for cell in listCells:
+            val = self.distance(cell.getPosX, cell.getPosY)
+            if val < lowestMovement:
+                bestCaseToGo = i
+            i += 1
+        return bestCaseToGo
